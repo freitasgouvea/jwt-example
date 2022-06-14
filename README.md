4289e472071daddb433718e2036bccb146365431
Flavio Gouvea <flaviogouveaneto@gmail.com>
2022-06-14 12:05:29 -0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# jwt-example
+## jwt-example
+
+This repository contains examples of javascript functions to encrypt and decrypt data using jwt.
+
+Clone this repository and run:
+
+```
+npm i
+```
+
+Set the secret key to be used by hash algorithm:
+
+```
+SECRET_KEY='test'
+```
+
+And run the script:
+
+```
+node index.js
+```
+
+For more details, view `index.js` file.
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+// data to be encrypted
+const data = { 
+  client: '1', 
+  address: '0x8B3A08b22d25C60e4b2BfD984e331568ECa4C299', 
+  role: 'user' 
+}
+
+// function that encrypt data using jwt
+function encrypt() {
+  // this jwt method encrypts data
+  const encryptedData = jwt.sign( 
+    data, // data to be encrypted
+    process.env.SECRET_KEY, // secret key to be used by hash algorithm
+    { algorithm: 'HS256' } // algotrithm, in this case, HS256 (HMAC using SHA-256 hash algorithm)
+  )
+  return encryptedData
+}
+
+// function that call encrypt function and decrypt data using jwt
+function decrypt() {
+  const accessToken = encrypt()
+  // this jwt method decrypts data
+  const decryptedData = jwt.verify(
+    accessToken, // access_token that contains the data to be decrypted
+    process.env.SECRET_KEY, // secret key to be used by hash algorithm, need to be the same of the encryption
+    { algorithm: 'HS256' } // algotrithm, need to be the same of the encryption
+  )
+  return decryptedData
+}
+
+console.log(decrypt())
